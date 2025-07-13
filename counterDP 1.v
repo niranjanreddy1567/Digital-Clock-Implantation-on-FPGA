8b2eb2f2c89bcc21af2d020f89c69191c63c9cf2
niranjanreddy1567 <niranjanreddyd56@gmail.com>
2025-07-13 21:22:14 +0530

    Update counterDP 1.v

--- counterDP 1.v
+++ counterDP 1.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/03/2023 10:44:54 PM
-// Design Name: 
-// Module Name: counterDP
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module counterDP(
 input clk,
 
